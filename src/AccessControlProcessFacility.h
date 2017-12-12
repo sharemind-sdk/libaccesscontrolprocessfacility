688582bc0435eb133a52c6fc77249172a77fe59b
Jaak Ristioja <jaak.ristioja@cyber.ee>
2017-12-12 13:10:58 +0200

    Initial commit.

---
+++ src/AccessControlProcessFacility.h
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) Cybernetica
+ *
+ * Research/Commercial License Usage
+ * Licensees holding a valid Research License or Commercial License
+ * for the Software may use this file according to the written
+ * agreement between you and Cybernetica.
+ *
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl-3.0.html.
+ *
+ * For further information, please contact us at sharemind@cyber.ee.
+ */
+
+#ifndef SHAREMIND_ACCESSCONTROLPROCESSFACILITY_H
+#define SHAREMIND_ACCESSCONTROLPROCESSFACILITY_H
+
+#include <cstddef>
+#include <memory>
+#include <sharemind/Concepts.h>
+#include <sharemind/Range.h>
+#include <sharemind/StringHashTablePredicate.h>
+#include <string>
+#include <type_traits>
+#include <utility>
+
+
+namespace sharemind {
+
+class AccessControlProcessFacility {
+
+public: /* Types: */
+
+    enum AccessType { Denied, Allowed, Unspecified };
+
+    using PreparedPredicateConcept = StringHashTablePredicate;
+
+    SHAREMIND_DEFINE_CONCEPT(ValidArgument) {
+        template <typename T>
+        auto check(T && t) -> SHAREMIND_REQUIRE_CONCEPTS(
+                        Callable(StringHasher const &, T &),
+                        Any(DecaysTo(T, std::string),
+                            ConvertibleTo(typename std::decay<T>::type,
+                                          char const *),
+                            InputRangeTo(T, char))
+                    );
+    };
+
+protected: /* types: */
+
+    struct PreparedPredicate {
+
+        PreparedPredicate() noexcept {}
+        PreparedPredicate(PreparedPredicate &&) noexcept = default;
+        PreparedPredicate(PreparedPredicate const &) noexcept = default;
+
+        virtual ~PreparedPredicate() noexcept {}
+
+        PreparedPredicate & operator=(PreparedPredicate &&) noexcept = default;
+        PreparedPredicate & operator=(PreparedPredicate const &) noexcept
+                = default;
+
+        virtual std::size_t hash() const noexcept = 0;
+        virtual bool operator()(std::string const &) const = 0;
+
+    };
+
+private: /* Types: */
+
+    template <typename Pred>
+    class PreparedPredicateImpl final: public PreparedPredicate {
+
+    public: /* Methods: */
+
+        template <typename ... Args>
+        PreparedPredicateImpl(Args && ... args)
+            : m_pred(std::forward<Args>(args)...)
+        {}
+
+        std::size_t hash() const noexcept final override
+        { return m_pred.hash(); }
+
+        virtual bool operator()(std::string const & str) const final override
+        { return m_pred(str); }
+
+    private: /* Fields: */
+
+        Pred m_pred;
+
+    };
+
+public: /* Methods: */
+
+    virtual ~AccessControlProcessFacility() noexcept {}
+
+    /**
+        \brief Checks for access of the given rules.
+
+        For each rule to check, the function takes two consecutive parameters,
+        each of which is either models PreparedPredicateConcept, decays to a
+        std::string, is convertible to (char const *) or is a range which can be
+        compared to a std::string using sharemind::rangeEqual(). The first of
+        each of these two is used to match the ruleset name, and the second is
+        used to match the object (rule).
+
+        \warning If comparing a given range with a std::string object using
+                 rangeEqual() throws, AccessType::Denied is returned.
+        \warning Character arrays as arguments are considered to be ranges,
+                 hence literal strings passed will contain the terminating NULL
+                 character. Use sharemind::asLiteralStringRange("literal") as a
+                 optimized workaround.
+     */
+    template <typename ... Args>
+    auto check(Args && ... args) const noexcept
+            -> SHAREMIND_REQUIRE_CONCEPTS_R(AccessType, ValidArgument(Args)...)
+    { return checkWithPredicates(getPredicate(std::forward<Args>(args))...); }
+
+protected: /* Methods: */
+
+    virtual AccessType checkWithPredicates(
+            PreparedPredicate const * const * ptrs,
+            std::size_t size) const noexcept = 0;
+
+private: /* Methods: */
+
+    AccessType checkWithPredicates() const noexcept
+    { return AccessType::Unspecified; }
+
+    template <typename ... Args>
+    auto checkWithPredicates(Args && ... args) const noexcept
+            -> typename std::enable_if<
+                    (sizeof...(Args) > 0u) && ((sizeof...(Args) % 2u) == 0u),
+                    AccessType
+                >::type
+    {
+        PreparedPredicate const * const ptrs[] = { std::addressof(args)... };
+        return checkWithPredicates(ptrs, sizeof...(Args) / 2u);
+    }
+
+    template <typename T,
+              typename Base =
+                    decltype(getOrCreateTemporaryStringHashTablePredicate(
+                                std::declval<T &&>()))>
+    static PreparedPredicateImpl<Base> getPredicate(T && t) {
+        return PreparedPredicateImpl<Base>(
+                    getOrCreateTemporaryStringHashTablePredicate(
+                        std::forward<T>(t)));
+    }
+
+}; /* class AccessControlProcessFacility { */
+
+} /* namespace sharemind */
+
+#endif /* SHAREMIND_ACCESSCONTROLPROCESSFACILITY_H */
---
+++ tests/TestInterface.cpp
@@ -0,0 +1,108 @@
+#include "../src/AccessControlProcessFacility.h"
+
+#include <cassert>
+#include <sharemind/TestAssert.h>
+#include <string>
+
+
+using namespace sharemind;
+
+namespace {
+
+char const POLICY[] = "The policy";
+char const OBJECT[] = "The object";
+
+class Facility final: public AccessControlProcessFacility {
+
+private: /* Methods: */
+
+    AccessType checkWithPredicates(
+            PreparedPredicate const * const * ptrs,
+            std::size_t size) const noexcept final override
+    {
+        if (!size)
+            return AccessType::Unspecified;
+        assert(ptrs);
+        AccessType r = AccessType::Unspecified;
+        try {
+            for (;;) {
+                PreparedPredicate const & policyPredicate = **ptrs;
+                PreparedPredicate const & objectPredicate = **++ptrs;
+                if (policyPredicate(m_policy) && objectPredicate(m_object))
+                    r = AccessType::Allowed;
+                if (!--size)
+                    break;
+                ++ptrs;
+            }
+        } catch (...) {
+            return AccessType::Denied;
+        }
+        return r;
+    }
+
+    std::string const m_policy{POLICY};
+    std::string const m_object{OBJECT};
+
+};
+
+template <typename Policy, typename Object>
+Facility::AccessType test(Policy && policy, Object && object) {
+    return Facility().check(std::forward<Policy>(policy),
+                            std::forward<Object>(object));
+}
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunneeded-member-function"
+#endif
+struct ThrowingRange {
+    constexpr static char const * begin() { return nullptr; }
+    constexpr static char const * end() { return nullptr; }
+    static std::size_t size() { throw 42; }
+};
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+} // anonymous namespace
+
+#define TA(...) \
+        SHAREMIND_TESTASSERT(test(__VA_ARGS__) == Facility::AccessType::Allowed)
+#define TD(...) \
+        SHAREMIND_TESTASSERT(test(__VA_ARGS__) == Facility::AccessType::Denied)
+#define TU(...) \
+    SHAREMIND_TESTASSERT(test(__VA_ARGS__) == Facility::AccessType::Unspecified)
+#define LIT(s) asLiteralStringRange(s)
+#define NTCS(s) static_cast<char const *>(s)
+#define STR(s) str_ ## s
+#define THROW ThrowingRange()
+
+int main() {
+    std::string const & str_POLICY(POLICY);
+    std::string const & str_OBJECT(OBJECT);
+    TD(THROW, THROW);
+    TD(THROW, OBJECT);
+    TD(THROW, LIT(OBJECT));
+    TD(THROW, NTCS(OBJECT));
+    TD(THROW, STR(OBJECT));
+    TU(POLICY, THROW);
+    TU(POLICY, OBJECT);
+    TU(POLICY, LIT(OBJECT));
+    TU(POLICY, NTCS(OBJECT));
+    TU(POLICY, STR(OBJECT));
+    TD(LIT(POLICY), THROW);
+    TU(LIT(POLICY), OBJECT);
+    TA(LIT(POLICY), LIT(OBJECT));
+    TA(LIT(POLICY), NTCS(OBJECT));
+    TA(LIT(POLICY), STR(OBJECT));
+    TD(NTCS(POLICY), THROW);
+    TU(NTCS(POLICY), OBJECT);
+    TA(NTCS(POLICY), LIT(OBJECT));
+    TA(NTCS(POLICY), NTCS(OBJECT));
+    TA(NTCS(POLICY), STR(OBJECT));
+    TD(STR(POLICY), THROW);
+    TU(STR(POLICY), OBJECT);
+    TA(STR(POLICY), LIT(OBJECT));
+    TA(STR(POLICY), NTCS(OBJECT));
+    TA(STR(POLICY), STR(OBJECT));
+}
