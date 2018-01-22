2cd9f99e44f7344463993c1f2b281084a4475638
Jaak Ristioja <jaak.ristioja@cyber.ee>
2018-01-22 15:04:10 +0200

    tests/TestInterface.cpp: Improved tests related to ranges throwing excepions.
    Previously, some cases were not caught.

--- tests/TestInterface.cpp
+++ tests/TestInterface.cpp
@@ -67,9 +67,16 @@ TestAccessResult test(Policy && policy, Object && object) {
 #pragma clang diagnostic ignored "-Wunneeded-member-function"
 #endif
 struct ThrowingRange {
-    constexpr static char const * begin() { return nullptr; }
-    constexpr static char const * end() { return nullptr; }
+    static char const * begin() { throw 42; }
+    static char const * end() { throw 42; }
+    constexpr static std::size_t size() noexcept { return 42u; }
+    std::size_t m_size;
+};
+struct ThrowingRange2 {
+    std::string::const_iterator begin() const noexcept { return m_str.begin(); }
+    std::string::const_iterator end() const noexcept { return m_str.end(); }
     static std::size_t size() { throw 42; }
+    std::string const & m_str;
 };
 #ifdef __clang__
 #pragma clang diagnostic pop
@@ -86,32 +93,46 @@ struct ThrowingRange {
 #define LIT(s) asLiteralStringRange(s)
 #define NTCS(s) static_cast<char const *>(s)
 #define STR(s) str_ ## s
-#define THROW ThrowingRange()
+#define PTHROW ThrowingRange{str_POLICY.size()}
+#define OTHROW ThrowingRange{str_OBJECT.size()}
+#define PTHROW2 ThrowingRange2{str_POLICY}
+#define OTHROW2 ThrowingRange2{str_OBJECT}
 
 int main() {
     std::string const & str_POLICY(POLICY);
     std::string const & str_OBJECT(OBJECT);
-    TE(THROW, THROW);
-    TE(THROW, OBJECT);
-    TE(THROW, LIT(OBJECT));
-    TE(THROW, NTCS(OBJECT));
-    TE(THROW, STR(OBJECT));
-    TU(POLICY, THROW);
+    TE(PTHROW, OTHROW);
+    TE(PTHROW, OTHROW2);
+    TE(PTHROW, OBJECT);
+    TE(PTHROW, LIT(OBJECT));
+    TE(PTHROW, NTCS(OBJECT));
+    TE(PTHROW, STR(OBJECT));
+    TE(PTHROW2, OTHROW);
+    TE(PTHROW2, OTHROW2);
+    TE(PTHROW2, OBJECT);
+    TE(PTHROW2, LIT(OBJECT));
+    TE(PTHROW2, NTCS(OBJECT));
+    TE(PTHROW2, STR(OBJECT));
+    TU(POLICY, OTHROW);
+    TU(POLICY, OTHROW2);
     TU(POLICY, OBJECT);
     TU(POLICY, LIT(OBJECT));
     TU(POLICY, NTCS(OBJECT));
     TU(POLICY, STR(OBJECT));
-    TE(LIT(POLICY), THROW);
+    TE(LIT(POLICY), OTHROW);
+    TE(LIT(POLICY), OTHROW2);
     TU(LIT(POLICY), OBJECT);
     TA(LIT(POLICY), LIT(OBJECT));
     TA(LIT(POLICY), NTCS(OBJECT));
     TA(LIT(POLICY), STR(OBJECT));
-    TE(NTCS(POLICY), THROW);
+    TE(NTCS(POLICY), OTHROW);
+    TE(NTCS(POLICY), OTHROW2);
     TU(NTCS(POLICY), OBJECT);
     TA(NTCS(POLICY), LIT(OBJECT));
     TA(NTCS(POLICY), NTCS(OBJECT));
     TA(NTCS(POLICY), STR(OBJECT));
-    TE(STR(POLICY), THROW);
+    TE(STR(POLICY), OTHROW);
+    TE(STR(POLICY), OTHROW2);
     TU(STR(POLICY), OBJECT);
     TA(STR(POLICY), LIT(OBJECT));
     TA(STR(POLICY), NTCS(OBJECT));
